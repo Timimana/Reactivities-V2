e146f7d2b0adbf00943b2800f02765e8d589a421
Timimana <timothyimana@gmail.com>
2025-06-29 19:06:29 +0100

    End of section 4

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -17,4 +17,8 @@ public class Activity
     public double Latitude { get; set; }
     public double Longitude { get; set; }
 
+    public static implicit operator System.Diagnostics.Activity(Activity v)
+    {
+        throw new NotImplementedException();
+    }
 }
